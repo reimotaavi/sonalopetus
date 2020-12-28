d872a116c87fd233c9ceba5c9882cc5fa8e806f1
uqbit <74680333+uqbit@users.noreply.github.com>
2020-12-28 16:48:52 +0200

    prefikspuu ja tipu andmestruktuur

--- sonalopetus.cpp
+++ sonalopetus.cpp
@@ -1,4 +1,33 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+// andmestruktruur puu tipu andmetega
+template <class Silt>
+class Tipp {
+
+public:
+	Silt silt;
+	Tipp<Silt>* vanem;
+	vector<Tipp<Silt>*> lapsed;
+};
+
+// andmestruktruur prefikspuu tippude ahelana
+template <class Silt>
+class Prefikspuu {
+
+public:
+    Tipp<Silt>* juur;
+};
+
 int main(){
-    
+
+
+
+
+
+
     return 0;
 }
